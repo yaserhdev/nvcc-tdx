2f443a85a9953873ddcc0c5e486d48874e51808a
yaserhdev <148010203+yaserhdev@users.noreply.github.com>
2026-05-26 11:59:44 -0400

    Update client-portal-test.js

--- client-portal-test.js
+++ client-portal-test.js
@@ -1,12 +1,2 @@
-console.log('jQuery:', window.jQuery);
-
-
-(function ($) {
-    function init() {
-        console.log("Init works!");
-        if (window.location.pathname.toLowerCase() === '/sbtdclient/413/portal/1836/home-1') {
-            document.querySelector('div.pull-left.topLevelSearch.input-group').style.display = 'none';
-        };
-    };
-    $(init);
-})(window.jQuery);
+console.log("Testing 123...");
+console.log('Send help!');
