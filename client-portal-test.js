e24e906169b0356a4a6043ad2c3263ad1587fcc8
yaserhdev <148010203+yaserhdev@users.noreply.github.com>
2026-05-26 10:57:24 -0400

    Update client-portal-test.js
    Updating with jQuery

--- client-portal-test.js
+++ client-portal-test.js
@@ -1,14 +1,10 @@
 console.log("Hello world!");
 
-(function () {
+(function ($) {
     function init() {
         if (window.location.pathname.toLowerCase() === '/sbtdclient/413/portal/1836/home-1') {
             document.querySelector('div.pull-left.topLevelSearch.input-group').style.display = 'none';
         };
     };
-    if (document.readyState === 'loading') {
-        document.addEventListener('DOMContentLoaded', init);
-    } else {
-        init();
-    };
+    $(init);
 })();
