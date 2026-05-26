4edf5949f658d0d669114c927e534c5890619434
yaserhdev <148010203+yaserhdev@users.noreply.github.com>
2026-05-26 11:06:05 -0400

    Update client-portal-test.js
    Debugging logs

--- client-portal-test.js
+++ client-portal-test.js
@@ -2,6 +2,7 @@ console.log("Hello world!");
 
 (function ($) {
     function init() {
+        console.log("Init works!");
         if (window.location.pathname.toLowerCase() === '/sbtdclient/413/portal/1836/home-1') {
             document.querySelector('div.pull-left.topLevelSearch.input-group').style.display = 'none';
         };
