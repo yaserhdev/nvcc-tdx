6a8956a858340f5af6a70d3c8eb6d6b82696d4a1
yaserhdev <148010203+yaserhdev@users.noreply.github.com>
2026-05-26 10:29:13 -0400

    Update client-portal-test.js
    Hide top-level search bar on home page

--- client-portal-test.js
+++ client-portal-test.js
@@ -1,1 +1,9 @@
 console.log("Hello world!");
+
+(function () {
+    document.addEventListener('DOMContentLoaded', function () {
+        if (window.location.pathname.toLowerCase() === '/sbtdclient/413/portal/home') {
+            document.querySelector('div.pull-left.topLevelSearch.input-group').style.display = 'none';
+        }
+    });
+})();
