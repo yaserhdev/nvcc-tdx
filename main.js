caf2beb0764a8b942d885226be1942e09b608095
yaserhdev <148010203+yaserhdev@users.noreply.github.com>
2026-05-27 17:45:41 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 (function ($) {
     function init() {
         console.log('Init function invoked!');
-        console.log('Update works!');
+        console.log('Testing!');
         if (window.location.pathname.toLowerCase() === '/sbtdclient/413/portal/1836/home-1') {
             document.querySelector('div.pull-left.topLevelSearch.input-group').style.display = 'none';
         };
