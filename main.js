5cbfea7eabac2ab6bcae20d7b4ba635526ea71cf
yaserhdev <148010203+yaserhdev@users.noreply.github.com>
2026-05-28 07:20:04 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 (function ($) {
     function init() {
-        console.log('Init function invoked!');
-        console.log('Hello!');
-        if (window.location.pathname.toLowerCase() === '/sbtdclient/413/portal/1836/home-1') {
+        if (window.location.pathname.toLowerCase() === '/sbtdclient/413/portal/1836/home-1' || window.location.pathname.toLowerCase() === '/sbtdclient/413/portal/home') {
             document.querySelector('div.pull-left.topLevelSearch.input-group').style.display = 'none';
         };
     };
