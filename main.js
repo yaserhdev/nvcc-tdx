63b53d8734cf2be10baf4bb43d773e02197419a0
yaserhdev <148010203+yaserhdev@users.noreply.github.com>
2026-05-28 07:36:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-(function ($) {
-    function init() {
-        if (window.location.pathname.toLowerCase() === '/sbtdclient/413/portal/1836/home-1' || window.location.pathname.toLowerCase() === '/sbtdclient/413/portal/home/') {
-            document.querySelector('div.pull-left.topLevelSearch.input-group').style.display = 'none';
-        };
+(function () {
+    if (window.location.pathname.toLowerCase() === '/sbtdclient/413/portal/1836/home-1' || window.location.pathname.toLowerCase() === '/sbtdclient/413/portal/home/') {
+        document.querySelector('div.pull-left.topLevelSearch.input-group').style.display = 'none';
     };
-    $(init);
-})(window.jQuery);
+})();
