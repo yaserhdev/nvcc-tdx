448d6aa1a8a9542382a71fce8df8ad9a5900bb7e
yaserhdev <148010203+yaserhdev@users.noreply.github.com>
2026-05-28 07:25:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 (function ($) {
     function init() {
-        if (window.location.pathname.toLowerCase() === '/sbtdclient/413/portal/1836/home-1' || window.location.pathname.toLowerCase() === '/sbtdclient/413/portal/home') {
+        if (window.location.pathname.toLowerCase() === '/sbtdclient/413/portal/1836/home-1' || window.location.pathname.toLowerCase() === '/sbtdclient/413/portal/home/') {
             document.querySelector('div.pull-left.topLevelSearch.input-group').style.display = 'none';
         };
     };
