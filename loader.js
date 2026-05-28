5c788aaf6d85cf2c2059ad0c650e5bf7ab2b5b4b
yaserhdev <148010203+yaserhdev@users.noreply.github.com>
2026-05-28 07:55:03 -0400

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,5 +1,4 @@
 (function() {
-    console.log('Loader function invoked!');
     var script = document.createElement('script');
     script.src = 'https://cdn.jsdelivr.net/gh/yaserhdev/nvcc-tdx/main.js?v=' + Date.now();
     document.head.appendChild(script);
