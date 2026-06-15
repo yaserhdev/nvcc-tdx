67fc1d2a5b76e62f761f219dc3c6096e02292047
yaserhdev <148010203+yaserhdev@users.noreply.github.com>
2026-06-15 12:13:51 -0400

    Delete loader_v6.1.js

--- loader_v6.1.js
+++
@@ -1,5 +0,0 @@
-(function() {
-    var script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/gh/yaserhdev/nvcc-tdx@main/main_v6.1.js?v=' + Date.now();
-    document.head.appendChild(script);
-})();
