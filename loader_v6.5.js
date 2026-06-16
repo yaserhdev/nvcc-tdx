2314632382f9bd01aeefb0551df6d46101343a9d
yaserhdev <148010203+yaserhdev@users.noreply.github.com>
2026-06-16 07:30:05 -0400

    Add files via upload

---
+++ loader_v6.5.js
@@ -0,0 +1,5 @@
+(function() {
+    var script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/gh/yaserhdev/nvcc-tdx@main/main_v6.5.js?v=' + Date.now();
+    document.head.appendChild(script);
+})();
